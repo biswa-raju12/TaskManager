867bd7e9f83caa84db14a0a88737b9fc41318db0
biswa-raju12 <biswaraju12@gmail.com>
2022-08-12 10:43:03 +0530

    Updates

---
+++ precache-manifest.cd7e20f637e649c3c98a328c38109576.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b8a283d2f56f7921edad79a39f94b",
+    "url": "/TaskManager/index.html"
+  },
+  {
+    "revision": "b4755035a63473073427",
+    "url": "/TaskManager/static/js/2.e2ae73b9.chunk.js"
+  },
+  {
+    "revision": "b8e1d17d3b8ad44f4df7",
+    "url": "/TaskManager/static/js/main.0e0c2351.chunk.js"
+  },
+  {
+    "revision": "f225c75f7616ab5bd37f",
+    "url": "/TaskManager/static/js/runtime~main.ae49988c.js"
+  }
+]);
